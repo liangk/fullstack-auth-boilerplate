443b8848a3ba113c749a69541c9c4efc5cff241f
liangk <liangk.tw@gmail.com>
2025-09-30 12:09:26 +1000

    Add emailVerified column

--- backend/prisma/migrations/0001_init/migration.sql
+++ backend/prisma/migrations/0001_init/migration.sql
@@ -7,6 +7,7 @@ CREATE TABLE "User" (
   "email" TEXT NOT NULL,
   "password" TEXT NOT NULL,
   "name" TEXT,
+  "emailVerified" BOOLEAN NOT NULL DEFAULT false,
   "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
   "updatedAt" TIMESTAMP(3) NOT NULL,
   "tokenVersion" INTEGER NOT NULL DEFAULT 0,
